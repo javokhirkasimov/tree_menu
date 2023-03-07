f3c09354e7a938ccfb75a51cafcdd3cff4d80dff
Javokhir <kasimovjavohir01@icloud.com>
2023-03-07 08:52:55 +0500

    second update

--- menu/templatetags/menu_tag.py
+++ menu/templatetags/menu_tag.py
@@ -6,7 +6,7 @@ register = template.Library()
 
 @register.inclusion_tag('tree_view_render.html')
 def draw_menu(menu_name):
-    obj = MenuElements.objects.filter(menu__name=menu_name)
+    obj = MenuElements.objects.filter(menu__name=menu_name, parent=None)
     if obj:
         return {'menu_name': menu_name, 'menu_elements': obj}
     return {"menu_name": menu_name, "menu_elements": None}
